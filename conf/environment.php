abd4d04bea1beb688a8c492d7f40c2da1b099e5d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-12-17 22:02:26 +0100

    fix

--- conf/environment.php
+++ conf/environment.php
@@ -5,25 +5,14 @@
 |--------------------------------------------------------------------------
  */
 
-if(is_string(Getenv('TYPE')) && Getenv('TYPE') == "docker") {
-    define('DATABASE_HOST', Getenv('DATABASE_HOST'));
-    define('DATABASE_PORT', filter_var(Getenv('DATABASE_PORT'), FILTER_VALIDATE_INT));
-    define('DATABASE_NAME', Getenv('DATABASE_NAME'));
-    define('DATABASE_USER', Getenv('DATABASE_USER'));
-    define('DATABASE_PASSWORD', Getenv('DATABASE_PASSWORD'));
-    define('DATABASE_PREFIX', Getenv('DATABASE_PREFIX')); // This must have a trailing underscore. Example: qr_
-    define('DATABASE_CHARSET', Getenv('DATABASE_CHARSET'));
-    define('TYPE', Getenv('TYPE'));
-    define('BASE_URL', Getenv('BASE_URL'));
-} else {
-    define('DATABASE_HOST', "localhost");
-    define('DATABASE_PORT', "3306");
-    define('DATABASE_NAME', "__DB_NAME__");
-    define('DATABASE_USER', "__DB_USER__");
-    define('DATABASE_PASSWORD', "__DB_PWD__");
-    define('DATABASE_PREFIX', "qr_"); // This must have a trailing underscore. Example: qr_
-    define('DATABASE_CHARSET', "utf8");
-}
+define('DATABASE_HOST', "localhost");
+define('DATABASE_PORT', "3306");
+define('DATABASE_NAME', "__DB_NAME__");
+define('DATABASE_USER', "__DB_USER__");
+define('DATABASE_PASSWORD', "__DB_PWD__");
+define('DATABASE_PREFIX', "qr_"); // This must have a trailing underscore. Example: qr_
+define('DATABASE_CHARSET', "utf8");
+
 /*
 |--------------------------------------------------------------------------
 | INSTALLATION WITHOUT CONTAINER
